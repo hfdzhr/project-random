62fe2a9b038551b9c11e9f24b80ce11c48f6e301
hafidalazhar25 <21221753116@smkassalaambandung.sch.id>
2022-03-29 13:58:51 +0700

    Mod Saweria-Design

---
+++ HTML/saweria-design/script.js
@@ -0,0 +1,11 @@
+function detail() {
+    location.assign('https://saweria.co/faq')
+}
+
+function login() {
+    location.assign('https://saweria.co/login')
+}
+
+function daftar() {
+    location.assign('https://saweria.co/daftar')
+}
