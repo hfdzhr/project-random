af887690a32fc0bc2060a00640bfa6c48b40d27f
hafidalazhar25 <21221753116@smkassalaambandung.sch.id>
2022-04-26 14:06:21 +0700

    Add Icon

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -6,7 +6,12 @@ const rl = readline.createInterface({
 
 rl.question('Siapa nama kamu ? ', function (name) {
     rl.question('Berapa umur mu ? ', function (umur) {
-        console.log(`Nama : ${name}, Umur : ${umur}`);
+        const nama = name
+        const age = parseInt(umur)
+
+        console.log("");
+        console.log(`Nama : ${nama}`);
+        console.log(`Umur : ${umur * 2}`);
         rl.close();
     });
 });
